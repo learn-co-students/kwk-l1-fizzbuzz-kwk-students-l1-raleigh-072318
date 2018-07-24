b51c7b80522c52ac83ca67e93e205723197765e5
aaahmad22 <aminahakeelahmad@gmail.com>
2018-07-24 19:05:51 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+def FizzBuzz (number)
+  if number % 3 == 0 && number % 5 == 0 
+  puts "FizzBuzz"
+  
+elsif number % 3 == 0
+  puts "Fizz"
+  
+elsif number % 5 == 0
+ puts "Buzz"
+# elsif number % 15 == 0
+#      return "FizzBuzzuzz"
+ end
+end
+puts fizzbuzz(15)
+puts fizzbuzz(3)
